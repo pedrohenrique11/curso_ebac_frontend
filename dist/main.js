ed849847701908b0066ce0e0c87d38bb5036d474
Pedro Henrique <76178088+pedrohenrique11@users.noreply.github.com>
2024-01-18 21:57:48 +0000

    exercicio completo

---
+++ dist/main.js
@@ -0,0 +1,13 @@
+"use strict";
+function multiplicar(a, b) {
+    return a * b;
+}
+;
+multiplicar(2, 5);
+function saudacao(nome) {
+    return `Olá ${nome}!`;
+}
+;
+saudacao("Pedro");
+console.log(multiplicar(2, 5));
+console.log(saudacao("Pedro"));
---
+++ main.ts
@@ -0,0 +1,12 @@
+function multiplicar(a: number, b:number): number {
+    return a * b
+};
+multiplicar(2,5);
+
+function saudacao(nome: string): string {
+    return `Olá ${nome}!`
+};
+saudacao("Pedro");
+
+console.log(multiplicar(2,5));
+console.log(saudacao("Pedro"));
